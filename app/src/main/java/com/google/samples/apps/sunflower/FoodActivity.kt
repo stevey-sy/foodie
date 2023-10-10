5967b6eeccfbe14ae9b6344a8ea5f52bcca773e1
steveydev <sinsy89@monkey-soft.com>
2023-10-10 23:36:15 +0900

    initial commit

---
+++ app/src/main/java/com/google/samples/apps/sunflower/FoodActivity.kt
@@ -0,0 +1,11 @@
+package com.google.samples.apps.sunflower
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food)
+    }
+}
---
+++ app/src/main/java/com/google/samples/apps/sunflower/FoodHomeViewPagerFragment.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.sunflower
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.samples.apps.sunflower.databinding.FragmentFoodHomeViewPagerBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FoodHomeViewPagerFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FoodHomeViewPagerFragment : Fragment() {
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        val binding = FragmentFoodHomeViewPagerBinding.inflate(inflater, container, false)
+//        val tabLayout = binding.tabs
+//        val viewPager = binding.viewPager
+//
+//
+//    }
+}
